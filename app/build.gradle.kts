875cd92033bc9412cc7809e6158141ed91188ee5
Codelamps <andrerizaldib@gmail.com>
2024-02-22 14:18:48 +0700

    feat : splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // SPLASH SCREEN DEPENDENCY
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
 }
